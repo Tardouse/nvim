29deb1cc313a2ace0a5b794497b96cf6f552048e
Jesse Wang <z872845991@163.com>
2025-06-14 14:32:15 +0800

    remove warning position_encoding

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
+-- ***************************************************
+-- fixed bug: position_encoding param is required in vim.lsp.util.make_position_params. Defaulting to position encoding of the first client.
+local util = require('vim.lsp.util')
+
+-- Store the original function
+local original_make_position_params = util.make_position_params
+
+-- Override make_position_params to provide default position_encoding
+util.make_position_params = function(win, offset_encoding)
+    -- Fallback to 'utf-16' if offset_encoding is not provided
+    offset_encoding = offset_encoding or 'utf-16'
+    return original_make_position_params(win, offset_encoding)
+end
+-- ***************************************************
+
 require("defaults")
 require("keymaps")
 require("plugins")
