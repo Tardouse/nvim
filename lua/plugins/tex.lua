d1dc2638c5912f64bfce9f544ff7a53fb7af064a
Jesse Wang <z872845991@gmail.com>
2025-07-07 10:28:20 +0800

    update vimtex

--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -1,12 +1,13 @@
 return {
-	"lervag/vimtex",
-	init = function()
-		vim.g.vimtex_view_method = "zathura"
-		vim.g.vimtex_mappings_enabled = 0
-		vim.g.vimtex_quickfix_ignore_filters = {
-			'Font shape',
-			"badness 10000",
-			"Package hyperref Warning",
-		}
-	end
+    "lervag/vimtex",
+    init = function()
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_mappings_enabled = 0
+        vim.g.vimtex_syntax_enabled = 0
+        vim.g.vimtex_quickfix_ignore_filters = {
+            'Font shape',
+            "badness 10000",
+            "Package hyperref Warning",
+        }
+    end
 }
