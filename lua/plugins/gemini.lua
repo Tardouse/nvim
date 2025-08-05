f6822718758b0a36b9a03a2d0b9b342375d0398e
Jesse Wang <z872845991@gmail.com>
2025-08-05 20:52:15 +0800

    update gemini

--- lua/plugins/gemini.lua
+++ lua/plugins/gemini.lua
@@ -1,30 +1,31 @@
 return {
-    'gera2ld/ai.nvim',
-    keys = { "<leader>sw" },
-    cmd = "AITranslate",
-    dependencies = 'nvim-lua/plenary.nvim',
-    opts = {
-        ---- AI's answer is displayed in a popup buffer
-        ---- Default behaviour is not to give it the focus because it is seen as a kind of tooltip
-        ---- But if you prefer it to get the focus, set to true.
-        result_popup_gets_focus = false,
-        ---- Override default prompts here, see below for more details
-        -- prompts = {},
-        ---- Default models for each prompt, can be overridden in the prompt definition
-        models = {
-            {
-                provider = 'gemini',
-                model = 'gemini-2.5-flash-lite-preview-06-17',
-                result_tpl = '## Gemini\n\n{{output}}',
-            },
-        },
+	"gera2ld/ai.nvim",
+	-- keys = { "<leader>sw" },
+	-- cmd = "AITranslate",
+	event = "VeryLazy",
+	dependencies = "nvim-lua/plenary.nvim",
+	opts = {
+		---- AI's answer is displayed in a popup buffer
+		---- Default behaviour is not to give it the focus because it is seen as a kind of tooltip
+		---- But if you prefer it to get the focus, set to true.
+		result_popup_gets_focus = false,
+		---- Override default prompts here, see below for more details
+		-- prompts = {},
+		---- Default models for each prompt, can be overridden in the prompt definition
+		models = {
+			{
+				provider = "gemini",
+				model = "gemini-2.5-flash-lite-preview-06-17",
+				result_tpl = "## Gemini\n\n{{output}}",
+			},
+		},
 
-        --- API keys and relavant config
-        gemini = {
-            api_key = '',
-            model = 'gemini-2.5-flash-lite-preview-06-17',
-            -- proxy = '',
-        },
-    },
-    vim.keymap.set({ 'n', 'v' }, '<leader>sw', ':AITranslate<CR>', { silent = true, nowait = true }),
+		--- API keys and relavant config
+		gemini = {
+			api_key = "",
+			model = "gemini-2.5-flash-lite-preview-06-17",
+			-- proxy = '',
+		},
+	},
+	vim.keymap.set({ "n", "v" }, "<leader>sw", ":AITranslate<CR>", { silent = true, nowait = true }),
 }
