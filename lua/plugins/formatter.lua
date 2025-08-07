0b75719f93eee881f81e83319139267e7c72cdcf
Jesse Wang <z872845991@gmail.com>
2025-08-07 15:54:36 +0800

    formatter: add switch and notify

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,11 +4,11 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		cmd = { "ConformInfo" },
 		config = function()
-			require("conform").setup({
-				format_on_save = {
-					lsp_fallback = true,
-					timeout_ms = 500,
-				},
+			local conform = require("conform")
+
+			local format_on_save_enabled = true
+
+			conform.setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
 					python = { "isort", "black" },
@@ -16,11 +16,89 @@ return {
 					typescript = { "prettier" },
 					json = { "prettier" },
 					go = { "gofmt" },
+					tex = { "latexindent" },
 				},
-				vim.keymap.set({ "n", "v" }, "<leader>ff", function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end, { desc = "Format file or range" }),
 			})
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function(args)
+					if not format_on_save_enabled then
+						return
+					end
+
+					local bufnr = args.buf
+					local formatters = conform.list_formatters(bufnr)
+					local formatter_names = vim.tbl_map(function(f)
+						return f.name
+					end, formatters)
+					local used = #formatter_names > 0 and table.concat(formatter_names, ", ") or "LSP"
+
+					vim.notify("Auto formatting with: " .. used, vim.log.levels.INFO, { title = "Conform" })
+
+					conform.format({
+						bufnr = bufnr,
+						async = false,
+						lsp_fallback = true,
+						timeout_ms = 500,
+					})
+				end,
+			})
+
+			-- formatter by keys
+			vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				local formatters = conform.list_formatters(bufnr)
+				local formatter_names = vim.tbl_map(function(f)
+					return f.name
+				end, formatters)
+				local used = #formatter_names > 0 and table.concat(formatter_names, ", ") or "LSP"
+
+				vim.notify("Formatting with: " .. used, vim.log.levels.INFO, { title = "Conform" })
+
+				conform.format({
+					async = true,
+					lsp_fallback = true,
+					timeout_ms = 500,
+				})
+			end, { desc = "Format file or range" })
+
+			-- toggle format on save
+			vim.keymap.set("n", "<leader>fc", function()
+				format_on_save_enabled = not format_on_save_enabled
+				vim.notify(
+					"Format on Save: " .. (format_on_save_enabled and "Enabled" or "Disabled"),
+					vim.log.levels.INFO,
+					{ title = "Conform" }
+				)
+			end, { desc = "Toggle format on save" })
 		end,
 	},
 }
+
+-- return {
+-- 	{
+-- 		"stevearc/conform.nvim",
+-- 		event = { "BufReadPre", "BufNewFile" },
+-- 		cmd = { "ConformInfo" },
+-- 		config = function()
+-- 			require("conform").setup({
+-- 				format_on_save = {
+-- 					lsp_fallback = true,
+-- 					timeout_ms = 500,
+-- 				},
+-- 				formatters_by_ft = {
+-- 					lua = { "stylua" },
+-- 					python = { "isort", "black" },
+-- 					javascript = { "prettier" },
+-- 					typescript = { "prettier" },
+-- 					json = { "prettier" },
+-- 					go = { "gofmt" },
+-- 					tex = { "latexindent" },
+-- 				},
+-- 				vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+-- 					require("conform").format({ async = true, lsp_fallback = true })
+-- 				end, { desc = "Format file or range" }),
+-- 			})
+-- 		end,
+-- 	},
+-- }
