b80d2d0d46d93869c4a1431d2768ff7d75700514
Jesse Wang <z872845991@gmail.com>
2025-10-30 10:12:56 +0800

    chore(vimlayouts): lualine use catppuccin theme

--- lua/plugins/vimlayouts.lua
+++ lua/plugins/vimlayouts.lua
@@ -25,10 +25,13 @@ return {
             require('lualine').setup {
                 options = {
                     icons_enabled = true,
-                    theme = 'dracula-nvim',
+                    -- theme = 'dracula-nvim',
+                    theme = 'catppuccin',
                     -- theme = 'auto',
-                    component_separators = { left = '', right = '' },
-                    section_separators = { left = '', right = '' },
+                    -- component_separators = { left = '', right = '' },
+                    -- section_separators = { left = '', right = '' },
+                    component_separators = { left = '', right = '' },
+                    section_separators = { left = '', right = '' },
                     disabled_filetypes = {
                         statusline = {},
                         winbar = {},
