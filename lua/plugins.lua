a4ce22255533f859c806dbac0cfff2493cd50014
Jesse Wang <z872845991@gmail.com>
2026-03-29 17:10:19 +0800

    chore(lazy): auto update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+if not vim.uv.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
@@ -50,5 +51,10 @@ require("lazy").setup({
 	require("plugins.copilot"),
 	require("plugins.translate"),
 	require("plugins.fun"),
-    require("plugins.tagbar"),
+	require("plugins.tagbar"),
+}, {
+	-- install = {
+	-- 	missing = false,
+	-- },
+	-- checker = { enabled = false },
 })
